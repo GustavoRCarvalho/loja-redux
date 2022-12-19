d1bfc27a2e0298af1257f9b3b8059ccbba9d649b
gustavo-paipe <gustavorafaeldecarvalho@gmail.com>
2022-12-19 04:43:51 -0300

    Fix images link on product page data

--- src/data/detailProduct.js
+++ src/data/detailProduct.js
@@ -1,17 +1,19 @@
+import { shirts } from './images'
+
 export function detailProduct(id) {
     return {
         "id" : id, 
-        "title": "Moletom Canguru Spirited BRANCO, VERDE e ROSA - Unissex", 
+        "title": "Camisa Manga Curta Padrão BRANCO, PRETO e ROSA - Unissex", 
         "price": 289.90,
         "oldPrice": 339.90,
         "installment": 12,
         "priceInstallment": 29.38,
         "imagesList": [
-            "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/078/806/products/011951-1dad8d4b53805770e616533948824608-1024-1024.png",
-            "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/078/806/products/021911-c242acaa8f9d53e1c416533948827592-1024-1024.png",
-            "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/078/806/products/04461-1e6dfb1795c04bf6a116533948823032-1024-1024.png",
-            "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/078/806/products/03511-03fde76c53cafb2d6c16533948826743-1024-1024.png",
-            "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/078/806/products/021911-c242acaa8f9d53e1c416533948827592-1024-1024.png",
+            shirts[0],
+            shirts[1],
+            shirts[2],
+            shirts[3],
+            shirts[4],
         ],
         "colors": {
             "title":"Selecione a Cor:",
